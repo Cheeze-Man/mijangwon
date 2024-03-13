ad3709b178d7759819e762e54e47ad7e137a83da
Cheeze-Man <pksam5046@naver.com>
2024-03-13 15:18:15 +0900

    Add: SWR config

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,4 +1,11 @@
+"use client";
+import useSWR from "swr";
+
 const FollowingBar = () => {
+  const { data, error, isLoading } = useSWR("/api/me");
+
+  console.log(data);
+
   return <p>FollowingBar</p>;
 };
 
