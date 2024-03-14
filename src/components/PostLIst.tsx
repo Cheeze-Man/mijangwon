8ff4e920376110e3997c5f0b6053b80fd0c223e8
cheezeman <pksam5046@naver.com>
2024-03-14 13:25:41 +0900

    Refactor: @sanity/image-url을 이용한 유저 프로필 이미지 최적화

--- src/components/PostLIst.tsx
+++ src/components/PostLIst.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { SimplePost } from "@/model/post";
+import { log } from "console";
 import useSWR from "swr";
 
 const PostLIst = () => {
   const { data: posts, isLoading } = useSWR<SimplePost[]>("/api/posts");
+  console.log(posts);
 
   return (
     <ul>{posts && posts.map((post) => <li key={post.id}>{post.text}</li>)}</ul>
