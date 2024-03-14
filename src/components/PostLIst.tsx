73391c525af4c3b9c6a6a9e21c3132e6ecefa214
cheezeman <pksam5046@naver.com>
2024-03-14 13:58:41 +0900

    Add: post list card - basic layout

--- src/components/PostLIst.tsx
+++ src/components/PostLIst.tsx
@@ -1,14 +1,31 @@
 "use client";
 import { SimplePost } from "@/model/post";
 import { log } from "console";
+import { GridLoader } from "react-spinners";
 import useSWR from "swr";
+import PostListCard from "./PostListCard";
 
 const PostLIst = () => {
   const { data: posts, isLoading } = useSWR<SimplePost[]>("/api/posts");
-  console.log(posts);
 
   return (
-    <ul>{posts && posts.map((post) => <li key={post.id}>{post.text}</li>)}</ul>
+    <section>
+      {isLoading && (
+        <div>
+          <GridLoader color="#8B5CF6" />
+        </div>
+      )}
+      {posts && (
+        <ul>
+          {posts &&
+            posts.map((post) => (
+              <li key={post.id}>
+                <PostListCard post={post} />
+              </li>
+            ))}
+        </ul>
+      )}
+    </section>
   );
 };
 
