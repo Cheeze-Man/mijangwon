bd8281114e6422ff7fa779d737fd7a97a5e34ef3
cheezeman <pksam5046@naver.com>
2024-03-20 13:03:04 +0900

    Refactor: 중복되는 세션 검증 로직 -> 하나의 함수로

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,19 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getPost } from "@/service/posts";
+import { withSessionUser } from "@/util/session";
 
 type Context = {
   params: { id: string };
 };
 
-export async function GET(req: NextRequest, context: Context) {
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
-
-  if (!user) {
-    return new Response("Authentication Error", { status: 401 });
-  }
-
-  return getPost(context.params.id).then((data) => NextResponse.json(data));
+export async function GET(_: NextRequest, context: Context) {
+  return withSessionUser(async () =>
+    getPost(context.params.id).then((data) => NextResponse.json(data))
+  );
 }
